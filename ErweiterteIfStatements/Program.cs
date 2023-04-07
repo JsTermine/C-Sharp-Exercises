49dfa78fd5e896400a9e937f77c7bcbe99321ad3
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-07 10:35:41 +0200

    Evaluating the State of Matter using If-Else and Ternary Conditionals

---
+++ ErweiterteIfStatements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ErweiterteIfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Temperatur des Wassers
+            int temperature = -5;
+
+            // Aggregatszustand
+            string stateofMatter;
+
+
+            if (temperature < 0)
+            {
+                stateofMatter = "fest";
+            }
+            else
+            {
+                stateofMatter = "flüssig";
+            }
+
+            Console.WriteLine("Aggregatszustand ist: {0}", stateofMatter);
+
+
+            temperature += 30;
+
+            // Erweitertes If-Statement (Verkürzt)
+            stateofMatter = temperature < 0 ? "fest" : "flüssig";
+
+            Console.WriteLine("Aggregatszustand ist: {0}", stateofMatter);
+
+
+            temperature += 100;
+
+            // (Herausforderung) Erweitertes If-Statement mit 2 Bedingungen
+            stateofMatter = temperature > 100 ? "Gas" : (stateofMatter = temperature < 0 ? "fest" : "flüssig");
+            Console.WriteLine("Aggregatszustand ist: {0}", stateofMatter);
+
+
+            Console.ReadKey(); 
+        }
+    }
+}
