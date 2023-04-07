ba64fc6231581391b68923f902fd64808981f9db
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-07 10:32:12 +0200

    DoWhile with name inputs from User

---
+++ DoWhileSchleife/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DoWhileSchleife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Namenzähler und Liste der Namen deklarieren
+            int nameCount = 0;
+            string arrayNames = "";
+
+            do
+            {
+                Console.WriteLine("Gib hier einen Namen ein:");
+                string nameOfAFriend = Console.ReadLine();
+
+                nameCount++;
+                arrayNames += nameOfAFriend + " ";
+            } while (nameCount < 5);
+
+
+            // Ausgabe der eingegebenen Namen
+            Console.WriteLine("Die eingegebene Namen sind: {0}", wholeText);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
