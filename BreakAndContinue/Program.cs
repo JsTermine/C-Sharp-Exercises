dfb1c34453f4773355176edb40250ed81e06638f
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-07 10:30:25 +0200

    If inside ForLoop skipping a number

---
+++ BreakAndContinue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BreakAndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zähler, welches Nummer 3 überspringt
+            for (int counter = 0; counter < 10; counter++)
+            {
+                if (counter == 3)
+                {
+                    Console.WriteLine("Bei drei überspringen!");
+                    continue;
+                }
+                Console.WriteLine(counter);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
