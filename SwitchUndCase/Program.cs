3e85a38897a320e936ad6afdd635d9d0951f1e70
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-07 10:41:44 +0200

    SwitchCase evaluating defined age

---
+++ SwitchUndCase/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SwitchUndCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 19;
+            
+            // Verzweigungen durch Switch
+            switch (age) 
+            {
+                case 15:
+                    Console.WriteLine("Zu jung zum Feiern");
+                    break;
+                case 25:
+                    Console.WriteLine("Erlaubt zum Feiern");
+                    break;
+
+                // Default: Wie Else, bei einem If-Statement
+                default:
+                    Console.WriteLine("Wie alt bist du eigentlich?");
+                    break;
+            }
+
+
+            // If-Else abbildung vom oberen Switch-Statement
+            if (age <= 15)
+            {
+                Console.WriteLine("Zu jung zum Feiern");
+            }
+            else if (age >= 25)
+            {
+                Console.WriteLine("Alles klar, ab gehts");
+            } 
+            else 
+            {
+                Console.WriteLine("Wie alt bist du eigentlich?");
+            }
+            
+
+            Console.ReadKey(); 
+        }
+    }
+}
