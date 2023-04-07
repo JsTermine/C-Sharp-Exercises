4b73d99328867ff4e754c0a57c0044f58727d503
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-07 10:40:34 +0200

    converting strings to Uppercase and Lowercase

---
+++ Strings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialisierung von Strings
+            string meinName = "Jason";
+            string meinNachname = "Termine";
+            string nachricht = "Hallo, mein Name ist: ";
+            
+            //Länge der nachricht mit der .Length property
+            int laenge = nachricht.Length;
+
+            //Eintrag alles gross schreiben
+            string allesGrossGeschrieben = nachricht.ToUpper();
+            string allesKleinGeschrieben = nachricht.ToLower();
+            
+            //Verwednung von concat für Zeichenketten/strings
+            Console.WriteLine(nachricht + meinName + meinNachname);
+            Console.WriteLine("Die länge der Nachricht ist: " + laenge);
+            
+            //Variable alles Gross oder alles Klein schreiben
+            Console.WriteLine(allesGrossGeschrieben);
+            Console.WriteLine(allesKleinGeschrieben);
+            
+            //Verwendung von {0}.
+            //{n} fügt den Eintrag ein, welches nach dem Komma kommt.
+            Console.WriteLine(nachricht + "ist {0} Zeichen lang", laenge);
+            Console.ReadKey();
+            
+        }
+    }
+}
