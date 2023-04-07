69fd0eebf2c0afcfd7d562e5484c197dd7674352
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-07 10:43:03 +0200

    Simple counter using While Loop

---
+++ WhileLoopD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhileLoopD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string enteredText = "";
+
+            while (enteredText.Equals("")) 
+            {
+                Console.WriteLine("Bitte drücke <Enter>, um Eins hochzuzählen:");
+                // Benutzereingabe
+                enteredText = Console.ReadLine();
+                Console.WriteLine("Aktuelle Anzahl Schüler: {0}", counter);
+                // Zählen hoch
+                counter++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
