2db16f0beb6be9fef4a2836a157c5d5da797b848
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-07 10:33:39 +0200

    Constructor Exercise with creating "Humans"

---
+++ ErsteKlasse/Human.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErsteKlasse
+{
+    // Mensch Klasse
+    // Eine Blaupause für einen Datentypen
+    class Human
+    {
+        // Membervariable Vorname
+        public string firstname;
+        // Membervariable Nachname
+        public string lastname;
+        // Membervariable Augenfarbe
+        string eyeColor;
+        // Membervariable Alter
+        int age;
+
+
+        public Human()
+        {
+            Console.WriteLine("Default Konstruktor aufgerufen");
+            age = 0;
+            eyeColor = "Braun";
+            firstname = "Unbekannt";
+            lastname = "Unbekannt";
+        }
+
+
+        // Parameterisierter Konstruktor
+        public Human(string firstname, string lastname, string eyeColor, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.eyeColor = eyeColor;
+            this.age = age;
+            Console.WriteLine("Neuer Human erstellt:");
+        }
+
+
+        // Member Methode
+        public void IntroduceMyself()
+        {
+
+            if (age == 1)
+            {
+                Console.WriteLine("Hi, ich bin {0} {1} und meine Augenfarbe ist {2} und bin {3} Jahr alt.", firstname, lastname, eyeColor, age);
+
+            }
+            else
+            { 
+                Console.WriteLine("Hi, ich bin {0} {1} und meine Augenfarbe ist {2} und bin {3} Jahre alt.", firstname, lastname, eyeColor, age);
+            
+            }
+               
+
+        }
+    }
+}
---
+++ ErsteKlasse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ErsteKlasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Erstellen eines Objektes meiner Human Klasse
+            Human max = new Human("Max", "Mustermann", "Braun", 1);
+            max.IntroduceMyself();
+
+            Human john = new Human("John","Doe", "Hellbraun", 22);
+            john.IntroduceMyself();
+
+            Human cron = new Human("Cron","Job", "Braun", 26);
+            cron.IntroduceMyself();
+
+            Human ronaldo = new Human("Ronaldo", "Da Costa", "Dunkelbraun", 25);
+            ronaldo.IntroduceMyself();
+
+            Human unbekannt = new Human();
+            unbekannt.IntroduceMyself();
+
+            Console.ReadKey();
+        }
+    }
+}
