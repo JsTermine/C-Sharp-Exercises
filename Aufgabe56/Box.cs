a42346e9d2aeab2ef78f723b91f7826da46ff7fe
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-08 11:00:48 +0200

    Simple exercise using custom properties

---
+++ Aufgabe56/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aufgabe56
+{
+    internal class Box
+    {
+        int height;
+        int length;
+
+        public int FrontSurface
+        { 
+            get 
+            {
+                return length*height;
+            }
+        }
+
+        public Box(int length, int height) 
+        {
+            this.length = length;
+            this.height = height;
+        }
+    }
+}
---
+++ Aufgabe56/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aufgabe56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(5, 3);
+            Console.WriteLine("Dei Vorderfläche vom Würfel ist: {0}", box.FrontSurface);
+
+            Console.ReadKey();
+        }
+    }
+}
