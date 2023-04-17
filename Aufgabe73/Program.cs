b2301714f3862e72edc9cfe290d707dd350384a6
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-17 16:21:17 +0200

    OOP Inheritance Exercise #1

---
+++ Aufgabe73/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aufgabe73
+{
+    class Program
+    {
+        // Herausforderung
+        // Füge eine erbende Klasse "VideoPost" hinzu, mit den Eigenschaften VideoURL unf Length
+        // Erstelle die benötigten Konstruktoren um ein VideoPost Objekt anzulegen
+        // Passe die ToString Methode entsprechen an
+        // Erstelle eine Instanz von VideoPost und verwende die ToString Methode darauf.
+
+
+        static void Main(string[] args)
+        {
+            VideoPost videoPost1 = new VideoPost("Fail Video", "John", "[URL Link]", true, 10);
+            Console.WriteLine(videoPost1.ToString());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aufgabe73/VideoPost.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Aufgabe73
+{
+    class VideoPost : Post
+    {
+        protected string VideoURL { get; set; }
+        protected int Length { get; set; }
+
+        public string Description{ get; set; }
+
+        public VideoPost(string videoURL, int length, string description)
+        {
+            this.VideoURL = videoURL;
+            this.Length = length;
+        }
+
+        public VideoPost() { }
+
+        // Default
+        public VideoPost(string title, string sentByUserName, string videoURL, bool isPublic, int length) 
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SentByUserName = sentByUserName;
+            this.IsPublic = isPublic;
+
+
+            this.VideoURL = videoURL;
+            this.Length = length;
+        }
+
+        public override string ToString() 
+        {
+            return String.Format("{0} - {1} - {2} - {3} - von {4}", this.ID, this.VideoURL, this.Length, this.Title, this.SentByUserName);
+        }
+
+    }
+}
