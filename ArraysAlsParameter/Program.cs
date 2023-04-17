02d0d40202c9141c54454526c2aa349a14b2356a
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-17 16:15:36 +0200

    Using Arrays as Parameters

---
+++ ArraysAlsParameter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace ArraysAlsParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentsGrades = new int[] { 15, 13, 8, 12, 6, 13 };
+            
+            double averageResult = GetAverage(studentsGrades);
+            
+            Console.ReadKey();
+        }
+
+        public static double GetAverage(int[] gradesArray)
+        {
+            int size = gradesArray.Length;
+            int sum = 0;
+            double average;
+
+            for (int i = 0; i < gradesArray.Length; i++)
+            {
+                sum += gradesArray[i];
+            }
+
+            average = (double)sum / size;
+
+            Console.WriteLine("Der Durchschnitt ist: {0}", average);
+
+            return average;
+        }
+    }
+}
