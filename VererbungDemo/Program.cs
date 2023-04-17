baf6b29bb90b04817c325c16ae8f8259a3eed713
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-17 16:29:05 +0200

    Update Program.cs

--- VererbungDemo/Program.cs
+++ VererbungDemo/Program.cs
@@ -9,7 +9,7 @@ namespace VererbungDemo
             Post post1 = new Post("Meine neuen Schuhe", true, "John");
             Console.WriteLine("Post ToString Methode: {0}", post1.ToString());
 
-            ImagePost bildPost1 = new ImagePost("Hier sind sie!", "John", "https://bilder.de/meineschuhe", true);
+            ImagePost bildPost1 = new ImagePost("Hier sind sie!", "John", "[URL Placeholder]", true);
             Console.WriteLine("Post ToString Methode: {0}", bildPost1.ToString());
             
             
