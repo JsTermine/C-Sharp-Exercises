15bd0f9c545ef6c4150f79bcc64b4f4c39e1a4cc
Jason Termine <130055600+JsTermine@users.noreply.github.com>
2023-04-30 12:57:07 +0200

    Read / Output contents of a Text file

---
+++ TextAusDateiLesen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAusDateiLesen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Liest Textinhalt von textFile.txt aus
+            string text = System.IO.File.ReadAllText(@"[Pfad]\textFile.txt");
+
+            Console.WriteLine("Die Textdatei beinhaltet folgenden Text: {0}", text);
+
+            // Andere herangehensweise
+            string[] zeilen = System.IO.File.ReadAllLines(@"[Pfad]\textFile.txt");
+
+            // Ausgabe von textFile.txt
+            Console.WriteLine("Die Inhalte der Datei sind: ");
+
+            foreach (string zeile in zeilen)
+            {
+                Console.WriteLine("\t " + zeile);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
